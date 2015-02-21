7db539512cffaa7ab044ce31bf3b02bda765cabc
psamanoq <psamanoq@hotmail.com>
2015-02-20 21:21:51 -0600

    Fix gap_init:2

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,6 +1,14 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "file": "plugins/org.apache.cordova.vibration/www/vibration.js",
+        "id": "org.apache.cordova.vibration.notification",
+        "merges": [
+            "navigator.notification",
+            "navigator"
+        ]
+    },
+    {
         "file": "plugins/org.apache.cordova.splashscreen/www/splashscreen.js",
         "id": "org.apache.cordova.splashscreen.SplashScreen",
         "clobbers": [
@@ -11,6 +19,7 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
+    "org.apache.cordova.vibration": "0.3.11",
     "org.apache.cordova.splashscreen": "0.3.4"
 }
 // BOTTOM OF METADATA
